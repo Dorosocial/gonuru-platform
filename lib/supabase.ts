085409681f792df5c817c0c97379fe33a43535f4
Dorosocial <168459168+Dorosocial@users.noreply.github.com>
2026-07-02 07:12:37 +0000

    Initialize Supabase client in supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://qoxgfmjwollknjporkpp.supabase.co',
+  'sb_publishable_xJgAKsCJ-74mj7b_ml8wug_lXZFid99'
+)
